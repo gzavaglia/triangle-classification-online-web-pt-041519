df7e6fd11ae5abf3e731518558f73384bd026a45
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-05-23 18:15:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
     # @sideC = sideC
     if sideA <= 0 || sideB <= 0 || sideC <= 0
         raise TriangleError
-    elsif sideA+sideB < sideC || sideA+sideC < sideB || sideC+sideB < sideA
+    elsif sideA+sideB <= sideC || sideA+sideC <= sideB || sideC+sideB <= sideA
           raise TriangleError
       else 
           @sideA = sideA
