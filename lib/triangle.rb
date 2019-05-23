a4551d9c20384ba1d9c46da9b891b9b8aba97810
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-05-23 18:16:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,18 +3,15 @@ class Triangle
   attr_accessor :sideA, :sideB, :sideC
   
   def initialize(sideA, sideB, sideC)
-    # @sideA = sideA
-    # @sideB = sideB
-    # @sideC = sideC
     if sideA <= 0 || sideB <= 0 || sideC <= 0
-        raise TriangleError
+      raise TriangleError
     elsif sideA+sideB <= sideC || sideA+sideC <= sideB || sideC+sideB <= sideA
-          raise TriangleError
-      else 
-          @sideA = sideA
-          @sideB = sideB
-          @sideC = sideC
-      end
+      raise TriangleError
+    else 
+      @sideA = sideA
+      @sideB = sideB
+      @sideC = sideC
+    end
   end
   
   def kind 
@@ -24,32 +21,10 @@ class Triangle
       return :isosceles
     elsif @sideA != @sideB && @sideA != @sideC && @sideB != @sideC
       return :scalene
-    # elsif @sideA <= 0 || @sideB <= 0 || @sideC <= 0
-    #   begin
-    #     raise TriangleError
-    #   rescue TriangleError => error
-    #       puts error.message1
-    #   end
-    #   elsif @sideA+@sideB < @sideC || @sideA+@sideC < @sideB || @sideC+@sideB < @sideA
-    #     begin
-    #       raise TriangleError
-    #     rescue TriangleError => error
-    #         puts error.message2
-    #     end
     end
   end
-  
-  
-  
+
   class TriangleError < StandardError
-    # def message1
-    #   "Sides must be larger than 0"
-    # end
-    
-    # def message2
-    #   "Triangle Inequality"
-    # end
-    
   end
   
 end
